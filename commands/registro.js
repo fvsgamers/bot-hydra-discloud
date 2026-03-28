765b282ff281838bc94a724cd4cadcf7f5b60120
fvsgamers <fvsgamers@gmail.com>
2026-03-28 15:00:51 -0300

    Update registro.js

--- commands/registro.js
+++ commands/registro.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { 
+  SlashCommandBuilder, 
+  ActionRowBuilder, 
+  ButtonBuilder, 
+  ButtonStyle 
+} = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,6 +11,27 @@ module.exports = {
     .setDescription('Abrir painel de recrutamento'),
 
   async execute(interaction) {
+
+    // IDs dos cargos que PODEM usar o comando
+    const cargosPermitidos = [
+      '1483885950928027748',
+      '1483886170495516734',
+      '1485735984967716976'
+    ];
+
+    const membro = interaction.member;
+
+    // Verifica se o usuário tem algum dos cargos
+    const temPermissao = membro.roles.cache.some(role => 
+      cargosPermitidos.includes(role.id)
+    );
+
+    if (!temPermissao) {
+      return interaction.reply({
+        content: '❌ Você não tem permissão para usar este comando.',
+        ephemeral: true
+      });
+    }
 
     const button = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
